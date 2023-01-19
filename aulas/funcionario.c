21dbc78f8cbf984327d881ccdf3f801006a27016
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-01-19 18:41:32 -0300

    Update funcionario.c

--- aulas/funcionario.c
+++ aulas/funcionario.c
@@ -10,25 +10,19 @@ void imprime(TFunc *func) {
     printf("%d", func->cod);
     printf("\nNome: ");
     printf("%s", func->nome);
-    printf("\nCPF: ");
-    printf("%s", func->cpf);
-    printf("\nData de Nascimento: ");
-    printf("%s", func->data_nascimento);
     printf("\nSalário: ");
     printf("%4.2f", func->salario);
     printf("\n**********************************************");
 }
 
 // Cria funcionario. Lembrar de usar free(func)
-TFunc *funcionario(int cod, char *nome, char *cpf, char *data_nascimento, double salario) {
+TFunc *funcionario(int cod, char *nome, double salario) {
     TFunc *func = (TFunc *) malloc(sizeof(TFunc));
     //inicializa espaço de memória com ZEROS
     if (func) memset(func, 0, sizeof(TFunc));
     //copia valores para os campos de func
     func->cod = cod;
     strcpy(func->nome, nome);
-    strcpy(func->cpf, cpf);
-    strcpy(func->data_nascimento, data_nascimento);
     func->salario = salario;
     return func;
 }
@@ -38,8 +32,6 @@ void salva(TFunc *func, FILE *out) {
     fwrite(&func->cod, sizeof(int), 1, out);
     //func->nome ao invés de &func->nome, pois string já é ponteiro
     fwrite(func->nome, sizeof(char), sizeof(func->nome), out);
-    fwrite(func->cpf, sizeof(char), sizeof(func->cpf), out);
-    fwrite(func->data_nascimento, sizeof(char), sizeof(func->data_nascimento), out);
     fwrite(&func->salario, sizeof(double), 1, out);
 }
 
@@ -52,17 +44,13 @@ TFunc *le(FILE *in) {
         return NULL;
     }
     fread(func->nome, sizeof(char), sizeof(func->nome), in);
-    fread(func->cpf, sizeof(char), sizeof(func->cpf), in);
-    fread(func->data_nascimento, sizeof(char), sizeof(func->data_nascimento), in);
     fread(&func->salario, sizeof(double), 1, in);
     return func;
 }
 
 // Retorna tamanho do funcionario em bytes
-int tamanho() {
+int tamanho_registro() {
     return sizeof(int)  //cod
            + sizeof(char) * 50 //nome
-           + sizeof(char) * 15 //cpf
-           + sizeof(char) * 11 //data_nascimento
            + sizeof(double); //salario
 }
