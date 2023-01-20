841da1236392787b2673538d23f286a6a337dd5b
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-01-20 09:40:23 -0300

    Update funcionario.c

--- aulas/funcionario.c
+++ aulas/funcionario.c
@@ -1,33 +1,62 @@
-#ifndef FUNCIONARIO_H_INCLUDED
-#define FUNCIONARIO_H_INCLUDED
-
+#include "funcionario.h"
+#include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
-
-#include <stdlib.h>
-#include <stdio.h>
-
-typedef struct Funcionario {
-    int cod;
-    char nome[50];
-    double salario;
-} TFunc;
+#include <stdarg.h>
 
 // Imprime funcionario
-void imprime(TFunc *func);
+void imprime(TFunc *func) {
+    printf("**********************************************");
+    printf("\nFuncionario de código ");
+    printf("%d", func->cod);
+    printf("\nNome: ");
+    printf("%s", func->nome);
+    printf("\nSalário: ");
+    printf("%4.2f", func->salario);
+    printf("\n**********************************************");
+}
 
-// Cria funcionario. Lembrar de usar free(funcionario)
-TFunc *funcionario(int cod, char *nome, double salario);
+// Cria funcionario. Lembrar de usar free(func)
+TFunc *funcionario(int cod, char *nome, double salario) {
+    TFunc *func = (TFunc *) malloc(sizeof(TFunc));
+    //inicializa espaço de memória com ZEROS
+    if (func) memset(func, 0, sizeof(TFunc));
+    //copia valores para os campos de func
+    func->cod = cod;
+    strcpy(func->nome, nome);
+    func->salario = salario;
+    return func;
+}
 
 // Salva funcionario no arquivo out, na posicao atual do cursor
-void salva(TFunc *func, FILE *out);
+void salva(TFunc *func, FILE *out) {
+    fwrite(&func->cod, sizeof(int), 1, out);
+    //func->nome ao invés de &func->nome, pois string já é ponteiro
+    fwrite(func->nome, sizeof(char), sizeof(func->nome), out);
+    fwrite(&func->salario, sizeof(double), 1, out);
+}
 
 // Le um funcionario do arquivo in na posicao atual do cursor
 // Retorna um ponteiro para funcionario lido do arquivo
-TFunc *le(FILE *in);
+TFunc *le(FILE *in) {
+    TFunc *func = (TFunc *) malloc(sizeof(TFunc));
+    if (0 >= fread(&func->cod, sizeof(int), 1, in)) {
+        free(func);
+        return NULL;
+    }
+    fread(func->nome, sizeof(char), sizeof(func->nome), in);
+    fread(&func->salario, sizeof(double), 1, in);
+    return func;
+}
 
 // Retorna tamanho do funcionario em bytes
-int tamanho_registro();
+int tamanho_registro() {
+    return sizeof(int)  //cod
+           + sizeof(char) * 50 //nome
+           + sizeof(double); //salario
+}
 
-
-#endif // FUNCIONARIO_H_INCLUDED
+int tamanho_arquivo(FILE *arq) {
+    fseek(arq, 0, SEEK_END);
+    int tam = trunc(ftell(arq) / tamanho_registro());
+    return tam;
+}
