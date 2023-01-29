49bad8cad13225c209bc34c9fa8bc489cf7053e1
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-01-29 15:28:49 -0300

    Create geracao_particoes.h

---
+++ praticaI/geracao_particoes.h
@@ -0,0 +1,16 @@
+#ifndef GERACAO_PARTICOES_H_INCLUDED
+#define GERACAO_PARTICOES_H_INCLUDED
+
+#include "lista.h"
+
+#include <stdio.h>
+
+void classificacao_interna(char *nome_arquivo_entrada, Lista *nome_arquivos_saida, int M);
+
+// Executa o algoritmo de geracao de particoes por Selecao com Substituicao
+// nome_arquivo_entrada: nome do arquivo de entrada
+// nome_arquivo_saida: lista encadeada com nomes de arquivos de saida
+// M: tamanho do array em memoria para manipulacao dos registros
+void selecao_com_substituicao(char *nome_arquivo_entrada, Lista *nome_arquivos_saida, int M);
+
+#endif // GERACAO_PARTICOES_H_INCLUDED
