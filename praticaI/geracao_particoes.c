684e6116163ba12a16445c1d422da6ba45291241
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-02-08 12:58:03 -0300

    Update geracao_particoes.c

--- praticaI/geracao_particoes.c
+++ praticaI/geracao_particoes.c
@@ -69,3 +69,91 @@ void selecao_com_substituicao(char *nome_arquivo_entrada, Lista *nome_arquivos_s
 {
     return;
 }
+
+void selecao_com_substituicao(char * nome_arquivo_entrada, Lista * nome_arquivos_saida, int M) {
+    FILE * arq; //pointer to input file
+    int fim = 0;
+
+    //open input file for reading
+    if ((arq = fopen(nome_arquivo_entrada, "rb")) == NULL) {
+        printf("Error opening input file\n");
+    } else {
+        //Ler primeiro funcionario
+        TFunc *cin = le(arq);
+
+        TFrozen *v[M]; //array to store records from input file
+        for (int i = 0; i < M; i++) {
+            v[i] = (TFrozen*)malloc(sizeof(TFrozen));
+        }
+        int i = 0;
+
+        //Ler M registros do arquivo para a memória
+        while (!feof(arq) && i < M) {
+            v[i]->func = cin;
+            cin = le(arq);
+            i++;
+        }
+
+        //adjust M if input file ended before filling the array v
+        if (i != M) {
+            M = i;
+        }
+
+        int frozen = 0;
+        TFunc *menor;
+        //Loop enquanto ainda tem registro descongelados
+        // Precisa de um loop aqui para abrir nova partição
+        //open output partition for writing
+        while (!fim) {
+            char *nome_particao = nome_arquivos_saida->nome;
+            nome_arquivos_saida = nome_arquivos_saida->prox;
+            FILE *p;
+
+            while (M - frozen > 0) {
+                if ((p = fopen(nome_particao, "wb")) == NULL) {
+                    printf("Error creating output partition\n");
+                } else {
+                    //find the record with the smallest key
+                    int ind_min = 0;
+                    for (int j = 1; j < M; j++) {
+                        if (v[j]->func->cod < v[ind_min]->func->cod && !v[j]->frozen) {
+                            ind_min = j;
+                        }
+                    }
+                    menor = v[ind_min]->func;
+                    //write the record with the smallest key to output partition
+                    salva(menor, p);
+
+                    //replace the record with the smallest key with the next record from input file
+                    if (!feof(arq)) {
+                        cin = le(arq);
+                        v[ind_min]->func = cin;
+                        if (cin != NULL) {
+                            if (cin->cod < menor->cod) {
+                                v[ind_min]->frozen = 1;
+                                frozen++;
+                            }
+                        }
+
+                        continue;
+                    } else if (feof(arq) && M - frozen > 0) {
+                        continue;
+                    }
+                    //close output partition
+                    frozen++;
+                    fclose(p);
+                }
+                //unfreeze frozen records
+                for (i = 0; i < M; i++) {
+                    v[i]->frozen = 0;
+                }
+            }
+            if (feof(arq) && frozen < M) {
+                fim = 1;
+            }
+        }
+        for (i = 0; i < M; i++) {
+            free(v[i]);
+        }
+    }
+}
